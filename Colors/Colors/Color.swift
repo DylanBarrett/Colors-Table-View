9ff0bfaa02e9dec46db4425bae19b68abb5f1ba0
dylan <dcbk34@github.com>
2019-03-02 09:54:54 -0600

    addded color struct and set background color on cells

---
+++ Colors/Colors/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Dylan barrett on 3/2/19.
+//  Copyright © 2019 Dylan barrett. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+    
+}
+
